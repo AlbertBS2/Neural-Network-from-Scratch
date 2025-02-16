9d50e6b9d84373ed8b6ed4c2464bece375f3ff1e
AlbertBS2 <abargallos@gmail.com>
2025-02-16 13:17:09 +0100

    Fix: Minor organization issues

--- plot.py
+++ plot.py
@@ -17,11 +17,13 @@ def training_curve_plot(title, train_costs, test_costs, train_accuracy, test_acc
     lg=18
     md=13
     sm=9
+    
     fig, axs = plt.subplots(1, 2, figsize=(12, 4))
     fig.suptitle(title, y=1.15, fontsize=lg)
     sub = f'| Batch size:{batch_size} | Learning rate:{learning_rate} |'
     fig.text(0.5, 0.99, sub, ha='center', fontsize=md)
     x = range(1, len(train_costs)+1)
+
     axs[0].plot(x, train_costs, label=f'Final train cost: {train_costs[-1]:.4f}')
     axs[0].plot(x, test_costs, label=f'Final test cost: {test_costs[-1]:.4f}')
     axs[0].set_title('Cost', fontsize=md)
@@ -29,8 +31,7 @@ def training_curve_plot(title, train_costs, test_costs, train_accuracy, test_acc
     axs[0].set_ylabel('Cost', fontsize=md)
     axs[0].legend(fontsize=sm)
     axs[0].tick_params(axis='both', labelsize=sm)
-    # Optionally use a logarithmic y-scale
-    #axs[0].set_yscale('log')
+
     axs[1].plot(x, train_accuracy, label=f'Final train accuracy: {100*train_accuracy[-1]:.2f}%')
     axs[1].plot(x, test_accuracy, label=f'Final test accuracy: {100*test_accuracy[-1]:.2f}%')
     axs[1].set_title('Accuracy', fontsize=md)
